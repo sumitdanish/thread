7c822ed68bed5b3303bdca958e66fe1b0c7097de
sumit <sumit.polite@gmail.com>
2015-12-19 23:50:44 +0530

    Thread

---
+++ Core/src/Text.java
@@ -0,0 +1,26 @@
+
+public class Text extends abd implements abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void abv() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
+abstract class abd{
+	public abd(){
+		
+	}
+	public int i = 10;
+	abstract public void abv();
+}
+interface abc{
+	public int i = 20;
+}
